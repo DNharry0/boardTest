713d994394f86cb58c5a3a2909df81ae3a941a01
harry <harrykcfa@gmail.com>
2024-02-19 19:10:24 +0900

    feat: dynamic route setting

--- forum/app/page.js
+++ forum/app/page.js
@@ -1,22 +1,13 @@
 import { connectDB } from '@/util/database';
-import { MongoClient } from 'mongodb';
 
 export default async function Home() {
 
-  const client = await connectDB;
-  const db = client.db("forum");
-  let result = await db.collection("post").find().toArray()
-  console.log(result)
+  // const client = await connectDB;
+  // const db = client.db("forum");
+  // let result = await db.collection("post").find().toArray()
 
   return (
     <main>
-      {result.map((post) => (
-        <div key={post._id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-        </div>
-      )
-      )}
     </main>
   );
 }
