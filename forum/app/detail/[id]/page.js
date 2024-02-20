033002dbb75ca9b54a487691b4776ab38c11055e
harry <harrykcfa@gmail.com>
2024-02-20 22:47:58 +0900

    feat: prefetch test

--- forum/app/detail/[id]/page.js
+++ forum/app/detail/[id]/page.js
@@ -2,11 +2,9 @@ import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb"
 
 export default async function Detail(props){
-    const db = (await connectDB).db("forum")
-    let result = await db.collection("post").findOne({_id: new
-        ObjectId(props.params.id)})
-    // console.log(result)
-    console.log(props.params.id)
+    let db = (await connectDB).db('forum')
+    let result = await db.collection('post').findOne({_id : new ObjectId(props.params.id)});
+    console.log(result)
 
     return (
         <div>
